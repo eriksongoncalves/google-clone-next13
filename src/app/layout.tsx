55d95c7883fc811d07ce9f24076946bd392d58ec
Erikson Gonçalves Bezerra <erikson.bezerra@ibm.com>
2023-03-26 12:40:32 -0300

    chore: add head file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 import './globals.css'
 
-export const metadata = {
-  title: 'Google clone'
-}
-
 export default function RootLayout({
   children
 }: {
@@ -11,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="relative min-h-screen">{children}</body>
     </html>
   )
 }
